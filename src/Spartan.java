b8b2593bec6a5fda615f8a41f3a2d33481eababa
Elena Ilic <125409165+eilici@users.noreply.github.com>
2023-04-06 14:19:41 -0700

    Update Spartan.java

--- src/Spartan.java
+++ src/Spartan.java
@@ -1,24 +1,49 @@
 package Foodie;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+//EDIT HISTORY:
+//3-28-2023 Robert Veloya: uploaded class with attributes, getters, setters
+//4-6-2023 Elena Ilic: Parameterized constructor, more attributes, writeReview
 
 public class Spartan extends User{
 	private int placesVisited;
 	ArrayList<Preferences> foodPref = new ArrayList<>();
-	
+	String userName;
+	String password;
+	ArrayList<Reviews> prevReviews;
 
 	public Spartan() {
 		this.setUserID(null);
 		this.foodPref.replaceAll(null);
 		this.placesVisited = 0;
-		//what more do we need? 
+		prevReviews = new ArrayList<Reviews>;
 	}
+	
 	public Spartan(String userName, String password, Preferences[] foodiePref) {
-		/*
-		 * TODO parameterized constructor. idk what to put here yet.
-		 */
-
- }
+		this.userName = userName;
+		this.password = password;
+		this.foodPref = Arrays.asList(foodiePref);
+		prevReviews = new ArrayList<Reviews>;
+	 }
+	
+	//write a review and add it to the list of reviews of a restaurant
+	public void writeReview(Food food, Restaurant restaurant, String review, double rating, Preferences[] prefs){
+		//create a new object from parameters given by user
+		Review r = new Review(review, rating);
+		
+		//adds review to restaurant and updates its rating
+		restaurant.addReview(r);
+		restaurant.calculateRating();
+		
+		//adds food preferences to lists for overall restaurant AND food
+		for (Preference p: prefs){
+			restaurant.addFoodPref(p);
+			food.addFoodPref(p);
+			
+		}
+	}
+	
 	public int getPlacesVisited() {
 		return placesVisited;
 	}
