ece8a00f659e22fdbe800baedb596b75e0ce180d
Elena Ilic <125409165+eilici@users.noreply.github.com>
2023-05-02 19:56:30 -0700

    Update Spartan.java

--- src/Spartan.java
+++ src/Spartan.java
@@ -1,78 +1,84 @@
-package Foodie;
+package foodie;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 //EDIT HISTORY:
 //3-28-2023 Robert Veloya: uploaded class with attributes, getters, setters
 //4-6-2023 Elena Ilic: Parameterized constructor, more attributes, writeReview
+//4-30-2023 remove prev reviews
+import java.util.List;
 
-public class Spartan extends User{
-	private int placesVisited;
-	ArrayList<Preferences> foodPref = new ArrayList<>();
-	String userName;
-	String password;
-	ArrayList<Reviews> prevReviews;
+public class Spartan extends User {
+	private List<Preferences> foodPref;
+	private String userName;
+	private String password;
 
 	public Spartan() {
 		this.setUserID(null);
 		this.foodPref.replaceAll(null);
-		this.placesVisited = 0;
-		prevReviews = new ArrayList<Reviews>;
 	}
-	
-	public Spartan(String userName, String password, Preferences[] foodiePref) {
+
+	public Spartan(String userName, String password, Preferences[] foodPref) {
 		this.userName = userName;
 		this.password = password;
-		this.foodPref = Arrays.asList(foodiePref);
-		prevReviews = new ArrayList<Reviews>;
-	 }
-	
-	//write a review and add it to the list of reviews of a restaurant
-	public void writeReview(Food food, Restaurant restaurant, String review, double rating, Preferences[] prefs){
-		//create a new object from parameters given by user
-		Review r = new Review(review, rating);
-		
-		//adds review to restaurant and updates its rating
-		restaurant.addReview(r);
-		restaurant.calculateRating();
-		
-		//adds food preferences to lists for overall restaurant AND food
-		for (Preference p: prefs){
+		this.foodPref = Arrays.asList(foodPref);
+	}
+
+	// write a review and add it to the list of reviews of a restaurant
+	public void writeReview(Food food, Restaurant restaurant, String review, Preferences[] prefs) {
+		// adds review to restaurant
+		restaurant.addReview(review);
+
+		// adds food preferences to lists for overall restaurant AND food
+		for (Preferences p : prefs) {
 			restaurant.addFoodPref(p);
 			food.addFoodPref(p);
-			
+
 		}
 	}
-	
-	public int getPlacesVisited() {
-		return placesVisited;
-	}
 
-	public void setPlacesVisited(int placesVisited) {
-		this.placesVisited = placesVisited;
-	}
-
-	public ArrayList<Preferences> getFoodPref() {
+	public List<Preferences> getFoodPref() {
 		return foodPref;
 	}
 
 	public void setFoodPref(ArrayList<Preferences> foodPref) {
 		this.foodPref = foodPref;
 	}
-	
+
 	public void addPreference(Preferences fp) {
-		if(this.foodPref.contains(fp)) {
+		if (this.foodPref.contains(fp)) {
 			System.out.println("Already added! Try again.");
-		}else {
+		} else {
 			this.foodPref.add(fp);
 		}
 	}
-	
+
 	@Override
 	public String toString() {
 		/*
-		 * add 
+		 * add
 		 */
 		return "change later";
 	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setFoodPref(List<Preferences> foodPref) {
+		this.foodPref = foodPref;
+	}
+	
 }
