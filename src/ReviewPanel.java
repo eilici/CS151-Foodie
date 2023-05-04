b8d1b9c627e84ccbeb0581095061de3319cae187
Robert Veloya <80503404+jrveloya@users.noreply.github.com>
2023-05-03 22:41:14 -0700

    Added text area and submit button
    Please check if it works on your end. My eclipse is acting weird and cant access our repo.

--- src/ReviewPanel.java
+++ src/ReviewPanel.java
@@ -11,18 +11,21 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 //Created 4-30-2023 by Elena Ilic
+// Edited 5/3/23 by Robert Veloya : added review text area and functional (?) submit button.
 public class ReviewPanel extends JFrame implements ActionListener {
 
 	private Container c;
 	
+	private JTextArea txtrReview;
+	
 	private JLabel desc;
-	private Restaurant restaurant;
+	//private Restaurant restaurant;
 
 	private JButton submit;
 	
-	public ReviewPanel(Restaurant restaurant) {
+	public ReviewPanel(/*Restaurant restaurant*/) {
 		
-		this.restaurant = restaurant;
+		//this.restaurant = restaurant;
 		
 		setTitle("Make new Review");
 
@@ -39,16 +42,49 @@ public class ReviewPanel extends JFrame implements ActionListener {
 		desc = new JLabel("What did you think?");
 		desc.setFont(new Font("HelveticaNeue MediumCond", Font.PLAIN, 20));
 		desc.setForeground(new Color(0, 85, 168));
-		desc.setSize(300, 35);
+		desc.setSize(169, 35);
 		desc.setLocation(65, 20);
 		c.add(desc);
-
+		
+		txtrReview = new JTextArea();
+		txtrReview.setBorder(new LineBorder(new Color(0, 0, 0)));
+		txtrReview.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		txtrReview.setBounds(10, 53, 320, 235);
+		getContentPane().add(txtrReview);
+		
+		JButton btnNewButton = new JButton("SUBMIT");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(txtrReview.getText() == "") {
+					JOptionPane.showMessageDialog(null,"Please enter a response","Invalid response", JOptionPane.OK_OPTION);
+				}else {
+					ArrayList<String> review = new ArrayList<>();
+					review.add(txtrReview.getText());
+					JOptionPane.showMessageDialog(null, "Review Added!", "Success", JOptionPane.OK_OPTION);
+					ReviewPanel.this.dispose();
+				}
+			}
+		});
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.setBorder(null);
+		btnNewButton.setBackground(new Color(0, 85, 168));
+		btnNewButton.setFont(new Font("HelveticaNeue MediumCond", Font.PLAIN, 15));
+		btnNewButton.setBounds(108, 299, 113, 26);
+		getContentPane().add(btnNewButton);
+		
+		
 		setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		if(txtrReview.getText() == null) {
+			JOptionPane.showMessageDialog(null,"Please enter a response","Invalid response", JOptionPane.OK_OPTION);
+		}else {
+			Restaurant.addReview(txtrReview);
+			JOptionPane.showMessageDialog(null, "Review Added!", "Success", JOptionPane.OK_OPTION);
+			ReviewPanel.this.dispose();
+		}
 
 	}
 }
