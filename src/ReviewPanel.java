04a8256632f95b1c85d6947f6237ee75e582afc1
Elena Ilic <125409165+eilici@users.noreply.github.com>
2023-05-03 23:28:43 -0700

    Update ReviewPanel.java
    rewrote actions listener and wrote line to update list display

--- src/ReviewPanel.java
+++ src/ReviewPanel.java
@@ -1,34 +1,41 @@
 package foodie;
 
 import java.awt.Color;
+import java.awt.ComponentOrientation;
 import java.awt.Container;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+import javax.swing.border.LineBorder;
 
 //Created 4-30-2023 by Elena Ilic
 public class ReviewPanel extends JFrame implements ActionListener {
 
 	private Container c;
-	
+
 	private JTextArea txtrReview;
-	
+
 	private JLabel desc;
 	private Restaurant restaurant;
 
+	private MenuFrame menuframe;
+
 	private JButton submit;
-	
-	public ReviewPanel(Restaurant restaurant) {
-		
+
+	public ReviewPanel(Restaurant restaurant, MenuFrame menuframe) {
+		this.menuframe = menuframe;
 		this.restaurant = restaurant;
-		
+
 		setTitle("Make new Review");
 
-		//create window
+		// create window
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setBounds(100, 100, 356, 375);
 		setResizable(false);
@@ -44,43 +51,39 @@ public class ReviewPanel extends JFrame implements ActionListener {
 		desc.setSize(169, 35);
 		desc.setLocation(65, 20);
 		c.add(desc);
-		
+
 		txtrReview = new JTextArea();
 		txtrReview.setBorder(new LineBorder(new Color(0, 0, 0)));
 		txtrReview.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 		txtrReview.setBounds(10, 53, 320, 235);
 		getContentPane().add(txtrReview);
-		
+
 		JButton btnNewButton = new JButton("SUBMIT");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(txtrReview.getText() == "") {
-					JOptionPane.showMessageDialog(null,"Please enter a response","Invalid response", JOptionPane.OK_OPTION);
-				}else {
-					ArrayList<String> review = new ArrayList<>();
-					review.add(txtrReview.getText());
-					JOptionPane.showMessageDialog(null, "Review Added!", "Success", JOptionPane.OK_OPTION);
-					ReviewPanel.this.dispose();
-				}
-			}
-		});
+	
 		btnNewButton.setForeground(new Color(255, 255, 255));
 		btnNewButton.setBorder(null);
 		btnNewButton.setBackground(new Color(0, 85, 168));
 		btnNewButton.setFont(new Font("HelveticaNeue MediumCond", Font.PLAIN, 15));
 		btnNewButton.setBounds(108, 299, 113, 26);
+		
+		btnNewButton.addActionListener(this);
 		getContentPane().add(btnNewButton);
-		
-		
+
 		setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(txtrReview.getText() == null) {
-			JOptionPane.showMessageDialog(null,"Please enter a response","Invalid response", JOptionPane.OK_OPTION);
-		}else {
+		if (txtrReview.getText() == null) {
+			JOptionPane.showMessageDialog(null, "Please enter a response", "Invalid response", JOptionPane.OK_OPTION);
+		} 
+		else{
+			System.out.println("text: " + txtrReview.getText());
 			restaurant.addReview(txtrReview.getText());
+
+			// reset review list
+			menuframe.getReviewList().append("\n" + txtrReview.getText());
+			
 			JOptionPane.showMessageDialog(null, "Review Added!", "Success", JOptionPane.OK_OPTION);
 			ReviewPanel.this.dispose();
 		}
