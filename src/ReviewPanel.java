3372b24c631a8c492ed210f1ef6fc224c0f20c3b
Elena Ilic <125409165+eilici@users.noreply.github.com>
2023-05-03 20:53:37 -0700

    Update ReviewPanel.java

--- src/ReviewPanel.java
+++ src/ReviewPanel.java
@@ -2,19 +2,31 @@ package foodie;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 //Created 4-30-2023 by Elena Ilic
 public class ReviewPanel extends JFrame implements ActionListener {
 
 	private Container c;
+	
+	private JLabel desc;
+	private Restaurant restaurant;
 
-	public ReviewPanel() {
-		setTitle("What are your filters?");
+	private JButton submit;
+	
+	public ReviewPanel(Restaurant restaurant) {
+		
+		this.restaurant = restaurant;
+		
+		setTitle("Make new Review");
 
+		//create window
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setBounds(100, 100, 356, 375);
 		setResizable(false);
@@ -22,11 +34,21 @@ public class ReviewPanel extends JFrame implements ActionListener {
 
 		c = getContentPane();
 		c.setLayout(null);
+
+		// create description
+		desc = new JLabel("What did you think?");
+		desc.setFont(new Font("HelveticaNeue MediumCond", Font.PLAIN, 20));
+		desc.setForeground(new Color(0, 85, 168));
+		desc.setSize(300, 35);
+		desc.setLocation(65, 20);
+		c.add(desc);
+
+		setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
